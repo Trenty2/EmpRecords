71aaba4a4827b751c39d86d67e3893063d95be27
Trent Sargisson <trent8865@gmail.com>
2020-10-09 16:37:46 +0800

    UI modifications to pages

---
+++ EmpRecords/Models/Supervisor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpRecords.Models
+{
+    public class Supervisor
+    {
+        
+    }
+}
